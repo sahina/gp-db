55f8b18ff2b34b72c7406cbe1eb2f02c08866938
Altug Sahin <altug@aecoffice.com>
2015-04-28 07:49:08 -0400

    adding filter to clubs

--- index.js
+++ index.js
@@ -1,22 +1,9 @@
-var feed = require('feed-read');
-var _ = require('lodash');
-var moment = require('moment');
+var util = require('util');
+var uefa = require('./lib/uefa');
 
-feed("http://www.uefa.com/rssfeed/uefachampionsleague/rss.xml", function(err, articles) {
-  if (err) throw err;
+// console.log(util.inspect(uefa.clubs(), {
+//   showHidden: true,
+//   depth: null
+// }));
 
-  _.forEach(articles, function (article) {
-    // console.log(article);
-    console.log(article.title, moment(article.published).fromNow());
-  });
-
-  // Each article has the following properties:
-  // 
-  //   * "title"     - The article title (String).
-  //   * "author"    - The author's name (String).
-  //   * "link"      - The original article link (String).
-  //   * "content"   - The HTML content of the article (String).
-  //   * "published" - The date that the article was published (Date).
-  //   * "feed"      - {name, source, link}
-  // 
-});
+console.log(uefa.clubs());
