245cedf0c5af23165c0c20974f53c394c7c54f7b
Altug Sahin <altug.sahin@credit-suisse.com>
2015-05-05 16:20:57 -0400

    added loader

--- index.js
+++ index.js
@@ -1,21 +1,7 @@
-var util = require('util');
-var assert = require('assert');
-var _ = require('lodash');
-var MongoClient = require('mongodb').MongoClient;
-var uefa = require('./lib/uefa');
+var load = require('./lib/load');
+var uefa= require('./lib/uefa');
 
 // console.log(uefa.clubs());
-// console.log(uefa.associations());
+ console.log(uefa.associations());
 
-var url = 'mongodb://localhost:27017/gp-db';
-MongoClient.connect(url, function(err, db) {
-
-  var clubs = _.flatten(uefa.clubs());
-
-  db.collection('clubs').insert(clubs, function (err, result) {
-    console.log(err);
-    console.log(result);
-    db.close();
-  });
-
-});
+load();
