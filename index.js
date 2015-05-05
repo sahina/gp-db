07274368404d71212b05d4ae0df8431ae5fbc54e
Altug Sahin <altug.sahin@credit-suisse.com>
2015-05-05 16:31:23 -0400

    added mongoose

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var load = require('./lib/load');
-var uefa= require('./lib/uefa');
+var uefa = require('./lib/uefa');
 
-// console.log(uefa.clubs());
- console.log(uefa.associations());
+ console.log(uefa.clubs());
+console.log(uefa.associations());
 
-load();
+//load();
