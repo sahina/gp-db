7af81fd4a42541f12be0c6b455c7fc63ea7f530a
Altug Sahin <altug.sahin@credit-suisse.com>
2015-05-05 15:00:30 -0400

    fixed string encodings

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 var util = require('util');
+var assert = require('assert');
+var _ = require('lodash');
+var MongoClient = require('mongodb').MongoClient;
 var uefa = require('./lib/uefa');
 
-// console.log(util.inspect(uefa.clubs(), {
-//   showHidden: true,
-//   depth: null
-// }));
+// console.log(uefa.clubs());
+// console.log(uefa.associations());
 
-console.log(uefa.clubs());
-console.log(uefa.associations());
+var url = 'mongodb://localhost:27017/gp-db';
+MongoClient.connect(url, function(err, db) {
+
+  var clubs = _.flatten(uefa.clubs());
+
+  db.collection('clubs').insert(clubs, function (err, result) {
+    console.log(err);
+    console.log(result);
+    db.close();
+  });
+
+});
